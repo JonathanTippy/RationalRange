645f110f674598c84f0719806e8c2720c4b7d91b
Jonathan Tippy <jonathantippy@outlook.com>
2024-10-14 18:34:22 -0500

    Rational

---
+++ src/main/java/com/jonathantippy/FastRational/Rational.java
@@ -0,0 +1,81 @@
+package com.jonathantippy.FastRational;
+
+import java.math.BigInteger;
+
+public class Rational 
+{
+    private BigInteger numerator;
+    private BigInteger denomenator;
+
+    // Constructors with both numerator and denomenator
+    public Rational(BigInteger numerator, BigInteger denomenator) throws ArithmeticException {
+        this.numerator = numerator; 
+        if (!this.denomenator.equals(BigInteger.ZERO)) {
+            this.denomenator = denomenator;
+        } else {
+            throw new ArithmeticException("/ by zero");
+        }
+    }
+    public Rational(long numerator, long denomenator) throws ArithmeticException {
+        this.numerator = BigInteger.valueOf(numerator);
+        if (!(denomenator==0)) {
+            this.denomenator = BigInteger.valueOf(denomenator);
+        } else {
+            throw new ArithmeticException("/ by zero");
+        }
+    }
+
+    // Constructors with integers
+    public Rational(BigInteger numerator) {
+        this.numerator = numerator; 
+        this.denomenator = BigInteger.ONE;
+    }
+    public Rational(long numerator) {
+        this.numerator = BigInteger.valueOf(numerator);
+        this.denomenator = BigInteger.ONE;
+    }
+
+    // Adaptive constructors
+    public Rational(String fraction) throws ArithmeticException, IllegalArgumentException {
+
+        if (fraction.matches("^\\d+(/?\\d+)?$\n")) {
+            if (fraction.contains("/")) {
+                String[] terms = fraction.split("/");
+                numerator = new BigInteger(terms[0]);
+                if (!BigInteger.ZERO.equals(new BigInteger(terms[1]))) {
+                    denomenator = new BigInteger(terms[1]);
+                } else {
+                    throw new ArithmeticException("/ by zero");
+                }
+            } else {
+                numerator = new BigInteger(fraction);
+                denomenator = BigInteger.ONE;
+            }
+        } else {
+            throw new IllegalArgumentException("Not a fraction");
+        }
+
+        
+    }
+
+    // Accessors and mutators
+    public BigInteger getNumerator() {
+        return this.numerator;
+    }
+    public BigInteger getDenomenator() {
+        return this.denomenator;
+    }
+
+    // Display
+    @Override
+    public String toString() {
+        int numeratorSign = this.numerator.compareTo(BigInteger.ZERO);
+        int denomenatorSign = this.denomenator.compareTo(BigInteger.ZERO);
+        int sign = numeratorSign*denomenatorSign;
+        if (sign >= 0) {
+            return this.numerator.abs().toString() + "/" + this.denomenator.abs().toString();
+        } else {
+            return "-" + this.numerator.abs().toString() + "/" + this.denomenator.abs().toString();
+        }
+    }
+}
---
+++ src/test/java/com/jonathantippy/FastRational/RationalTest.java
@@ -0,0 +1,13 @@
+package com.jonathantippy.FastRational;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RationalTest {
+    @Test
+    public void stringPositiveTest() {
+        Rational r = new Rational("50/7");
+        String s = r.toString();
+        assertEquals(s, "50/7");
+    }
+}
