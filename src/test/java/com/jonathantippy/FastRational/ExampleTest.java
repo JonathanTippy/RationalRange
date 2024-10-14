6cc1f523cfcf2fa0e55c63f11be1091b457b31c1
Jonathan Tippy <jonathantippy@outlook.com>
2024-10-14 17:33:24 -0500

    setup testing

---
+++ src/test/java/com/jonathantippy/FastRational/ExampleTest.java
@@ -0,0 +1,12 @@
+package com.jonathantippy.FastRational;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ExampleTest {
+    @Test
+    public void testTests() {
+        int total = 1 + 1;
+        assertEquals(2, total);
+    }
+}
