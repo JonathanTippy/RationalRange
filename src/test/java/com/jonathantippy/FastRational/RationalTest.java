0434e9d6976d35c1786ddaded079a7b1df209907
Jonathan Tippy <jonathantippy@outlook.com>
2024-10-17 16:58:43 -0500

    add negative string test (broken)

--- src/test/java/com/jonathantippy/FastRational/RationalTest.java
+++ src/test/java/com/jonathantippy/FastRational/RationalTest.java
@@ -4,10 +4,18 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class RationalTest {
+    
     @Test
     public void stringPositiveTest() {
         Rational r = new Rational("50/7");
         String s = r.toString();
         assertEquals(s, "50/7");
+    }
+
+    @Test
+    public void stringNegativeTest() {
+        Rational r = new Rational("-50/7");
+        String s = r.toString();
+        assertEquals(s, "-50/7");
     }
 }
